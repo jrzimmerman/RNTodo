e8f4624c3ebde7faa86832d2815b41b2cc2c7a26
Justin Zimmerman <zimmy130@gmail.com>
2016-08-06 22:47:02 -0400

    todo native app

---
+++ src/app/Todo.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+export class Todo extends Component {
+  constructor() {
+    super();
+    console.warn('yoooo');
+    this.state = {
+      todos: [],
+      newTodo: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handleChange(text) {
+    this.setState({
+      newTodo: text
+    });
+  }
+
+  handlePress() {
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({
+      todos,
+      newTodo: ''
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          value={this.state.newTodo}
+          onChangeText={this.handleChange}
+        />
+        <TouchableOpacity>
+          <Text onPress={this.handlePress}>Add Todo</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.todos.map((todo, index) => (
+            <Text key={index}>{todo}</Text>
+          ))}
+        </View>
+      </View>
+    )
+  }
+}
